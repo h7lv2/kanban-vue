0932cd2b47ee6a324a84df070708d749c3b2e364
YokoMinazawa <83871837+YokoMinazawa@users.noreply.github.com>
2025-07-04 16:59:28 +0300

    Delete src/components/FieldCard.vue

--- src/components/FieldCard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1></h1>
-  <p></p>
-  <div>
-    <img src="./icons/edit.svg" alt="Отредактировать задачу" />
-    <img src="./icons/info.svg" alt="Полная информация по задаче" />
-  </div>
-</template>
