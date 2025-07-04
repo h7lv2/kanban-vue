513631d50475588d39472d67a412e070e88cb873
YokoMinazawa <83871837+YokoMinazawa@users.noreply.github.com>
2025-07-04 16:59:53 +0300

    Delete src/components/AppTopBar.vue

--- src/components/AppTopBar.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <input placeholder="Поиск по задачам..." />
-  <div>
-    <button>Аккаунт</button>
-    <button>Создать задачу</button>
-  </div>
-</template>
